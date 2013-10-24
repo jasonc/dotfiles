2f62d3c85722e8ab58cee60ae04a920cc3db366c
Jason Cannon <jason@thisidig.com>
2013-10-24 13:02:26 -0500

    Clean up if statements.

--- install.sh
+++ install.sh
@@ -2,18 +2,15 @@
 
 DOT_FILES_DIR=~/.dotfiles
 
-if [ -e ${DOT_FILES_DIR} ]
-then
+[ -e ${DOT_FILES_DIR} ] && {
   echo "${DOT_FILES_DIR} already exists.  Exiting."
   exit 1
-fi
+}
 
-git clone git://github.com/jasonc/dotfiles ${DOT_FILES_DIR}
-if [ $? -ne 0 ]
-then
-  echo "Git clone failed.  Exiting."
+git clone git://github.com/jasonc/dotfiles ${DOT_FILES_DIR} || {
+  echo 'Git clone failed.  Exiting.'
   exit 1
-fi
+}
 
 cd ${DOT_FILES_DIR}
 
